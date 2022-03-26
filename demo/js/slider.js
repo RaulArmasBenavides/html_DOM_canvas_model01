9425e4730e2fa1ea3c908e7f2154f6cca583402e
Raúl Armas <raularmasbx@gmail.com>
2022-03-25 22:11:28 -0500

    updates

--- demo/js/slider.js
+++ demo/js/slider.js
@@ -8,19 +8,36 @@ slider.insertAdjacentElement('afterbegin',sliderSectionLast);
 
 const Next  =  () => {
     let sliderSectionFirst = document.querySelectorAll(".slider_section")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.25s";
+    slider.style.transition = "all 0.5s";
 
     setTimeout(()=>{
         slider.style.transition ="none";
         slider.insertAdjacentElement('beforeend',sliderSectionFirst);
-        slider.style.marginLeft ="-100%";
-    },500);
-}
+        slider.style.marginLeft ="0";
+    },450);
+};
+
+const Back  =  () => {
+    let sliderSection = document.querySelectorAll(".slider_section");
+    let sliderSectionLast = document.querySelectorAll(".slider_section")[sliderSection.length -1];
+    slider.style.marginLeft = "0%";
+    slider.style.transition = "all 0.5s";
+    
+    setTimeout(()=>{
+       slider.style.transition ="none";
+      slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+      slider.style.marginLeft ="-100%";
+    },450);
+};
 
 btnRight.addEventListener('click',function()
 {
     Next();
+});
+
+btnLeft.addEventListener('click',function()
+{
+    Back();
 });
 
  
